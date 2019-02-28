246b46a2bac573c4602ba28a1f4f6bc4aa8b4839
Lou Lemarie <llemarie17@gmail.com>
2019-02-28 16:43:28 +0100

    ajout des relations entres entitées et update de la base de donnée

--- src/Entity/Bin.php
+++ src/Entity/Bin.php
@@ -31,6 +31,23 @@ class Bin
      */
     private $volume;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\City", inversedBy="bins")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $city;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\City", inversedBy="bins")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $Collection;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\City", cascade={"persist", "remove"})
+     */
+    private $Admin;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -68,6 +85,42 @@ class Bin
     public function setVolume(int $volume): self
     {
         $this->volume = $volume;
+
+        return $this;
+    }
+
+    public function getCity(): ?City
+    {
+        return $this->city;
+    }
+
+    public function setCity(?City $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCollection(): ?City
+    {
+        return $this->Collection;
+    }
+
+    public function setCollection(?City $Collection): self
+    {
+        $this->Collection = $Collection;
+
+        return $this;
+    }
+
+    public function getAdmin(): ?City
+    {
+        return $this->Admin;
+    }
+
+    public function setAdmin(?City $Admin): self
+    {
+        $this->Admin = $Admin;
 
         return $this;
     }
--- src/Entity/City.php
+++ src/Entity/City.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -25,6 +27,16 @@ class City
      * @ORM\Column(type="integer")
      */
     private $zip_code;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Bin", mappedBy="city")
+     */
+    private $bins;
+
+    public function __construct()
+    {
+        $this->bins = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -51,6 +63,37 @@ class City
     public function setZipCode(int $zip_code): self
     {
         $this->zip_code = $zip_code;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Bin[]
+     */
+    public function getBins(): Collection
+    {
+        return $this->bins;
+    }
+
+    public function addBin(Bin $bin): self
+    {
+        if (!$this->bins->contains($bin)) {
+            $this->bins[] = $bin;
+            $bin->setCity($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBin(Bin $bin): self
+    {
+        if ($this->bins->contains($bin)) {
+            $this->bins->removeElement($bin);
+            // set the owning side to null (unless already changed)
+            if ($bin->getCity() === $this) {
+                $bin->setCity(null);
+            }
+        }
 
         return $this;
     }
