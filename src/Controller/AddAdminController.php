0214d6e0e89bd04e9253169d16a31dd2569aa925
rodolphe <rodolphe76100@gmail.com>
2019-03-05 10:04:09 +0100

    ajout page d'inscription des admins

---
+++ src/Controller/AddAdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AddAdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/dashboard/addadmin", name="add_admin")
+     */
+    public function index()
+    {
+        return $this->render('add_admin.html.twig', [
+            'controller_name' => 'AddAdminController',
+        ]);
+    }
+}
