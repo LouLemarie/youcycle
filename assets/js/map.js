afce8b4a982cb9b8d34ed87e016151d9bcdb272e
ratelhugo <ratelhugo@gmail.com>
2019-03-07 16:39:48 +0100

    GEOLOC FONCTIONELLE FINALE WOWOW

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,39 +1,40 @@
 
 function initMap() {
-    var map = L.map('map').setView([45.7563851, 4.8248167], 12);
-    var osmLayer = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution: '© <a href= »http://osm.org/copyright »>OpenStreetMap</a> contributors',
-        maxZoom: 19
-    });
-    map.addLayer(osmLayer);
-    // var bennes= $.getJSON(parseJSON('{{ link }}'),function(dataBennes) {
-    //     var iconeBennes = L.icon({
-    //         iconUrl: './../build/glassLogo.png',
-    //         iconSize: [25, 25]
-    //     });
-    //     L.geoJson(dataBennes, {
-    //         pointToLayer: function (features, latlng) {
-    //             var marker = L.marker(latlng, {icon: iconeBennes});
-    //             return marker;
-    //         }
-    //     }).addTo(map);
-    // });
-    var baseLayers = {
-        'OpenStreetMap': osmLayer,
-    };
-    L.control.layers(baseLayers).addTo(map);
-
     var geoSuccess = function (position) { // Ceci s'exécutera si l'utilisateur accepte la géolocalisation
         startPos = position;
         userlat = startPos.coords.latitude;
         userlon = startPos.coords.longitude;
         console.log("lat: " + userlat + " - lon: " + userlon);
+        var map = L.map('map').setView([userlat,userlon], 12);
+        var osmLayer = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '© <a href= »http://osm.org/copyright »>OpenStreetMap</a> contributors',
+            minZoom: 2,
+            maxZoom: 22
+        });
+        map.addLayer(osmLayer);
+        // var bennes= $.getJSON(parseJSON('{{ link }}'),function(dataBennes) {
+        //     var iconeBennes = L.icon({
+        //         iconUrl: './../build/glassLogo.png',
+        //         iconSize: [25, 25]
+        //     });
+        //     L.geoJson(dataBennes, {
+        //         pointToLayer: function (features, latlng) {
+        //             var marker = L.marker(latlng, {icon: iconeBennes});
+        //             return marker;
+        //         }
+        //     }).addTo(map);
+        // });
+        var baseLayers = {
+            'OpenStreetMap': osmLayer,
+        };
+        L.control.layers(baseLayers).addTo(map);
     };
     var geoFail = function () { // Ceci s'exécutera si l'utilisateur refuse la géolocalisation
         console.log("refus");
     };
     // La ligne ci-dessous cherche la position de l'utilisateur et déclenchera la demande d'accord
     navigator.geolocation.getCurrentPosition(geoSuccess, geoFail);
+
 }
 
 window.onload = function(){
