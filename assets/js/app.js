dcb52f69a353415c92beaf6375de34548beac0d8
Lou Lemarie <llemarie17@gmail.com>
2019-03-12 11:43:35 +0100

    test change

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,44 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
+
+
+global.$ = global.jQuery = $;
+
+
+// CSS
+require('../css/base.css');
+require('../css/main.css');
+
+// Fonts
+require('../css/font-awesome/css/font-awesome.css');
+require('../css/font-awesome/fonts/FontAwesome.otf');
+require('../css/font-awesome/fonts/fontawesome-webfont.eot');
+require('../css/font-awesome/fonts/fontawesome-webfont.svg');
+require('../css/font-awesome/fonts/fontawesome-webfont.ttf');
+require('../css/font-awesome/fonts/fontawesome-webfont.woff');
+require('../css/font-awesome/fonts/fontawesome-webfont.woff2');
+
+// Micons
+require('../css/micons/fonts/icomoon.eot');
+require('../css/micons/fonts/icomoon.svg');
+require('../css/micons/fonts/icomoon.ttf');
+require('../css/micons/fonts/icomoon.woff');
+require('../css/micons/micons.css');
+
+// Javascript
+require('./modernizr.js');
+require('./pace.min.js');
+require('./jquery-3.2.1.min.js');
+require('./plugins.js');
+require('./main.js');
+require('./searchbar.js');
+
+
 require('../css/app.css');
 require('../js/map.js');
 
- global.$ = global.jQuery = $;
+global.$ = global.jQuery = $;
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
  const $ = require('jquery');
@@ -19,3 +53,4 @@ require('../js/map.js');
  require('leaflet.markercluster');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
