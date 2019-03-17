8cebd8f0547b4d32346cc51fdb6ebdf0edaf1fd1
HugoNF <ratelhugo@gmail.com>
2019-03-17 22:16:22 +0100

    il reste juste un petit problème et c'est la map finale

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,40 +1,60 @@
 window.onload = function () {
     console.log('Page chargée ');
 
-var map = L.map('mapid').setView([43.610819, 3.876656], 13);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiemFua2siLCJhIjoiY2p0NWthODUzMDU1bTN5bnZqd2syZWtjeiJ9.2BQH9higFVNhEeRDJ8kRrw'
-}).addTo(map);
+    var map = L.map('map').setView([43.610819, 3.876656], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiemFua2siLCJhIjoiY2p0NWthODUzMDU1bTN5bnZqd2syZWtjeiJ9.2BQH9higFVNhEeRDJ8kRrw'
+    }).addTo(map);
 
     geojson = L.geoJson(verre, {
         style: function (feature) {
             return feature.properties && feature.properties.style;
-        },
-        pointToLayer: function (feature, latlng) {
-            var marker =  L.marker(latlng)({
-                icon: {
-                    url: "http://maps.gstatic.com/intl/de_de/mapfiles/ms/micons/red-pushpin.png",
-                    size: {width: 26, height: 32},
-                    origin: {x: 0, y: 0},
-                    anchor: {
-                        x: "-10px",
-                        y: "-32px"
-                    }
-                }
-            });
-            marker.bindPopup('<b><u>Description de la benne</u></b><br>'
-                + '<b>Commune: </b>Montpellier<br>'
-                + '<b>La rue : </b>' + feature.properties.rue + '<br>'
-                + '<b>Coordonnées: </b>' + feature.geometry.coordinates + '<br>'
-
-            );
-            return marker ;
-
         }
+
     }).addTo(map);
+    var markersCluster = new L.MarkerClusterGroup({
+        iconCreateFunction: function(cluster) {
+            return L.divIcon({
+                html: cluster.getChildCount(),
+                className: 'mycluster',
+                iconSize: null
+            });
+        }
+    });
+    var marker = geojson.getLayers();
+
+    for ( var i = 0; i < marker.length; ++i )
+    {
+
+        var  myIcon = L.icon({
+            iconUrl: "build/img/glassLogo.png"
+        });
+
+        //   var marker = L.marker(latlng,{icon:myIcon});
+
+
+        // Detail du Popup
+
+            var popup ='<b><u>Description de la benne</u></b><br>'
+                + '<b>Adresse : </b>' + marker[i].feature.properties.adresse + '<br>'
+                + '<b>Commune:</b>' + marker[i].feature.properties.commune + '<br>'
+                + '<b>Code commune : </b>' + marker[i].feature.properties.code_com + '<br>'
+                + '<b>Capacité : La poubelle</b>' + '<br>'
+            ;
+
+
+        var m = L.marker( [marker[i]._latlng.lat, marker[i]._latlng.lng] , {icon: myIcon} )
+            .bindPopup( popup )
+        ;
+
+        markersCluster.addLayer( m );
+    }
+
+    map.addLayer( markersCluster );
+
 
     /*var popup = L.popup();
     function onMapClick(e) {
@@ -46,14 +66,14 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 
     map.locate({setView: false, watch: false}) /* This will return map so you can do chaining */
         .on('locationfound', function(e){
-            var marker = L.marker([e.latitude, e.longitude]).bindPopup('Vous êtes ici :)');
+          //  var marker = L.marker([e.latitude, e.longitude]).bindPopup('Vous êtes ici :)');
             var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
                 weight: 1,
                 color: 'green',
                 fillColor: '#cacaca',
                 fillOpacity: 0.2
             });
-            map.addLayer(marker);
+        //    map.addLayer(marker);
             map.addLayer(circle);
         })
         .on('locationerror', function(e){
@@ -61,10 +81,6 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
             alert("Location access denied.");
         });
 
-    map.on('click', onMapClick);
+    /*map.on('click', onMapClick);*/
 };
-
-
-
-
 
