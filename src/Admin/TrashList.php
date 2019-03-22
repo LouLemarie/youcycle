910680b5a2d6ecfcc81caf42ea231fead0856e6b
rodolphe <rodolphe76100@gmail.com>
2019-03-22 09:06:02 +0100

    Merge branch 'develop' of https://github.com/LouLemarie/youcycle into features/Dashboard
    # Conflicts:
    #	composer.lock
    #	symfony.lock
    accès list bennes

--- src/Admin/TrashList.php
+++ src/Admin/TrashList.php
@@ -4,6 +4,8 @@
 
  use Sonata\AdminBundle\Admin\AbstractAdmin;
  use Sonata\AdminBundle\Datagrid\ListMapper;
+ use Sonata\AdminBundle\Form\FormMapper;
+ use Sonata\Form\Validator\ErrorElement;
 
  final class TrashList extends AbstractAdmin
  {
@@ -12,6 +14,18 @@
          $listMapper
              ->add('title')
              ->add('draft')
+
          ;
      }
+     protected function configureFormFields(FormMapper $formMapper)
+     {
+        $formMapper
+            ->add('benne','localisation',[])
+            ->add('enable')
+            ->add('title')
+            ->add('abstract', null, ['required' => false])
+            ->add('content');
+
+     }
+
  }
